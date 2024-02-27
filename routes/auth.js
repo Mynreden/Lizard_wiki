7df90fb7dac36c7ed22e3ff1d6028f5b5e427378
Sultan A <aubakirov2004sultan@gmail.com>
2024-02-27 21:59:12 +0600

    changes

--- routes/auth.js
+++ routes/auth.js
@@ -5,24 +5,10 @@ import path from "path";
 
 const router = Router()
 const name = process.cwd();
-// upload files
-const storage = multer.diskStorage({ // in this line we define Strorage for multer
-    destination: (req, file, callback) => {
-        callback(null, "public/avatars") // callback(err: Error, destination: String)
-    },
-    filename: (req, file, callback) => {
-        const filename = Date.now() + '-' + file.originalname; // создаем новое имя файла, добавляя к оригинальному имени дату создания
-        callback(null, filename); // передаем новое имя файла в callback
-    }
-
-})
-
-export const upload = multer({storage}) // multer middlware uses form data files and transform it into req.file(upload.sindle()) 
-                                 // or req.files(upload.array())
 
 
 // Routes
-router.post('/register', upload.single('avatar'),  register) // route for registration
+router.post('/register', register) // route for registration
 router.post("/login", login) // that controller will work in localhost:3001/auth/login
 
 export default router;
--- routes/lizard.js
+++ routes/lizard.js
@@ -4,24 +4,9 @@ import path from "path";
 import {addLizard} from "../controllers/lizard.js"
 const router = Router()
 const name = process.cwd();
-// upload files
-const storage = multer.diskStorage({ // in this line we define Strorage for multer
-    destination: (req, file, callback) => {
-        callback(null, "public/lizards") // callback(err: Error, destination: String)
-    },
-    filename: (req, file, callback) => {
-        const filename = Date.now() + '-' + file.originalname; // создаем новое имя файла, добавляя к оригинальному имени дату создания
-        callback(null, filename); // передаем новое имя файла в callback
-    }
-
-})
-
-const upload = multer({storage}) // multer middlware uses form data files and transform it into req.file(upload.sindle()) 
-                                 // or req.files(upload.array())
 
 
-
-router.post("/", upload.array('images', 5) ,addLizard)
+router.post("/",addLizard)
 
 
 export default router;
